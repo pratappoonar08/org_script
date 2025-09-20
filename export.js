5e56ba1fec96102982f939f3a272a611b6bb7995
newben420 <wraith@wraith.wraith>
2025-09-20 16:35:39 +0100

    added exporting project templates

--- export.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ExportEngine = void 0;
-class ExportEngine {
-}
-exports.ExportEngine = ExportEngine;
-ExportEngine.route = (req, res) => {
-    const r = req.params.name;
-};
